0b914395bf420b8f1d5ba26c3e5c779815364e9e
abyubale <abyubale@gmail.com>
2023-12-27 14:41:14 +0530

    version-1

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducers from "../components/AddSubItem/AddSubSlice";
+import cartReducers from "../components/AddSubItem/addSubSlice";
 import appHeaderReducer from "../components/AppHeader/AppHeaderSlice";
 export const store = configureStore({
   reducer: {
